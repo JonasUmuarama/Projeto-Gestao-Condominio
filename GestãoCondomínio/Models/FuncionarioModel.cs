47196100f0c1e4989a5a6920a6d5b8a4e1d9b1f6
Jonas Santos <uportumu@gmail.com>
2022-05-02 20:47:57 -0300

    Alteração footer

---
+++ GestãoCondomínio/Models/FuncionarioModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestãoCondomínio.Models
+{
+    public class FuncionarioModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string RG { get; set; }
+        public string Telefone { get; set; }
+        public string Funcao { get; set; }
+    }
+}
