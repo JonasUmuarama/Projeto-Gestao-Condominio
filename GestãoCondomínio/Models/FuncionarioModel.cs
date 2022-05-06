05ca95c3fabe90f7834f51e60315a93ebffd9c20
Jonas Santos <uportumu@gmail.com>
2022-05-05 21:20:01 -0300

    Alteração Model Funcionário

--- GestãoCondomínio/Models/FuncionarioModel.cs
+++ GestãoCondomínio/Models/FuncionarioModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +10,14 @@ namespace GestãoCondomínio.Models
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        [Required(ErrorMessage = "Digite o Nome do funcionário")]
         public string RG { get; set; }
+        [Required(ErrorMessage = "Digite o RG do funcionário")]
         public string Telefone { get; set; }
+        [Required(ErrorMessage = "Digite o Telefone do funcionário")]
         public string Funcao { get; set; }
+        [Required(ErrorMessage = "Digite a Função do funcionário")]
         public string Descricao { get; set; }
+        [Required(ErrorMessage = "Digite a Descrição do funcionário")]
     }
 }
