270d015d817646ff92585c897839bbfeb77f73a5
Jonas Santos <uportumu@gmail.com>
2022-05-05 21:05:03 -0300

    Criação View Cadastrar Funcionário

--- GestãoCondomínio/Controllers/FuncionarioController.cs
+++ GestãoCondomínio/Controllers/FuncionarioController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GestãoCondomínio.Models;
+using GestãoCondomínio.Repositorio;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,46 @@ namespace GestãoCondomínio.Controllers
 {
     public class FuncionarioController : Controller
     {
+        private readonly IFuncionarioRepositorio _funcionarioRepositorio;
+
+        public FuncionarioController(IFuncionarioRepositorio funcionarioRepositorio)
+        {
+            _funcionarioRepositorio = funcionarioRepositorio;
+        }
         public IActionResult Index()
         {
+            List<FuncionarioModel> funcionarios = _funcionarioRepositorio.BuscarTodos();
+
+            return View(funcionarios);
+        }
+
+        public IActionResult Cadastrar()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(FuncionarioModel funcionario)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _funcionarioRepositorio.Adicionar(funcionario);
+                    TempData["MensagemSucesso"] = "Funcionário cadastrado com sucesso!";
+                    return RedirectToAction("Index");
+                }
+
+
+                return View(funcionario);
+            }
+            catch (Exception erro)
+            {
+
+                TempData["MensagemErro"] = $"Erro ao cadastrar, tente novamnete,detalhe do erro :{ erro.Message}";
+                return RedirectToAction("Index");
+            }
+
         }
     }
 }
