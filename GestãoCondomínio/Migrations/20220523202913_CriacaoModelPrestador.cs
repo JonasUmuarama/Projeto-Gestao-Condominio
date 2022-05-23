692a90b8fdf4241516ddf00e5131c9a40b43627d
Jonas Santos <uportumu@gmail.com>
2022-05-23 17:31:37 -0300

    Migration Prestador de Serviços realizada

---
+++ GestãoCondomínio/Migrations/20220523202913_CriacaoModelPrestador.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GestãoCondomínio.Migrations
+{
+    public partial class CriacaoModelPrestador : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Placa",
+                table: "Veiculos",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Prestadores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true),
+                    RG = table.Column<string>(nullable: true),
+                    CPF = table.Column<string>(nullable: true),
+                    Contato = table.Column<string>(nullable: true),
+                    Empresa = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prestadores", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Prestadores");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Placa",
+                table: "Veiculos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
