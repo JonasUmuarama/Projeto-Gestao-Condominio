07079b401d408538c0b6c81d9316d9db48557d2a
Jonas Santos <uportumu@gmail.com>
2022-05-23 19:42:58 -0300

    Alteração Controller Prestador

--- GestãoCondomínio/Controllers/PrestadorController.cs
+++ GestãoCondomínio/Controllers/PrestadorController.cs
@@ -1,4 +1,5 @@
-﻿using GestãoCondomínio.Repositorio;
+﻿using GestãoCondomínio.Models;
+using GestãoCondomínio.Repositorio;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -17,10 +18,98 @@ namespace GestãoCondomínio.Controllers
         }
         public IActionResult Index()
         {
+            List<PrestadorModel> prestadores = _prestadorRepositorio.BuscarTodos();
+            return View(prestadores);
+            
+        }
+
+        public IActionResult Cadastrar()
+        {
             return View();
         }
 
+        public IActionResult Editar(int id)
+        {
+            PrestadorModel prestador = _prestadorRepositorio.ListarPorId(id);
+            return View(prestador);
+        }
+
+        public IActionResult Apagar(int id)
+        {
+            PrestadorModel prestador = _prestadorRepositorio.ListarPorId(id);
+            return View(prestador);
+        }
+
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                bool apagado = _prestadorRepositorio.Deletar(id);
+                if (apagado)
+                {
+                    TempData["MensagemSucesso"] = "Prestador de Serviço excluído com sucesso!";
+                }
+                else
+                {
+                    TempData["MensagemErro"] = $"Erro ao excluir, tente novamente";
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Erro ao excluir, tente novamente, mais detalhes: {erro.Message} ";
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(PrestadorModel prestador)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _prestadorRepositorio.Adicionar(prestador);
+                    TempData["MensagemSucesso"] = "Prestador de Serviço cadastrado com sucesso!";
+                    return RedirectToAction("Index");
+                }
 
 
+                return View(prestador);
+            }
+            catch (Exception erro)
+            {
+
+                TempData["MensagemErro"] = $"Erro ao cadastrar, tente novamnete,detalhe do erro :{ erro.Message}";
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        [HttpPost]
+        public IActionResult Editar(PrestadorModel prestador)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _prestadorRepositorio.Atualizar(prestador);
+                    TempData["MensagemSucesso"] = "Prestador de Serviço alterado com sucesso!";
+                    return RedirectToAction("Index");
+                }
+
+                return View(prestador);
+            }
+            catch (Exception erro)
+            {
+
+                TempData["MensagemErro"] = $"Erro ao atualizar, tente novamnete,detalhe do erro :{ erro.Message}";
+                return RedirectToAction("Index");
+            }
+
+
+
+        }
+
+        }
     }
-}
