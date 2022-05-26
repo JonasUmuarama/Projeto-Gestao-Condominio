9f8b6eafd21bf0a83071d6fadd83088b4568279f
Jonas Santos <uportumu@gmail.com>
2022-05-26 12:43:06 -0300

    Criação Model Acesso

---
+++ GestãoCondomínio/Models/AcessoModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestãoCondomínio.Models
+{
+    public class AcessoModel
+    {
+        public int Id { get; set; }
+        
+        public string Veiculo { get; set; }
+        public string Condutor { get; set; }
+        public string Documento { get; set; }
+        public DateTime Entrada { get; set; }
+
+        public DateTime Saida { get; set; }
+
+        public string Destino { get; set; }
+
+
+    }
+}
