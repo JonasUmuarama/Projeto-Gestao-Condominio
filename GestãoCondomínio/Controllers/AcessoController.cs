3e5cc11814636d25a48d44a2c79ad24363919c63
Jonas Santos <uportumu@gmail.com>
2022-05-26 13:11:50 -0300

    Criação Controller Acesso

---
+++ GestãoCondomínio/Controllers/AcessoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestãoCondomínio.Controllers
+{
+    public class AcessoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
